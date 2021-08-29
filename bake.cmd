74a7ec91f568f87954e444d13ad950120ba75664
Tomasz Stachowiak <h3@h3.gd>
2021-08-29 18:24:04 +0200

    Add asset licenses

--- bake.cmd
+++ bake.cmd
@@ -7,7 +7,6 @@ set BAKE=target\release\bake
 %BAKE% --scene "assets/meshes/336_lrm/scene.gltf" --scale 0.01 -o 336_lrm
 %BAKE% --scene "assets/meshes/pica_pica_-_mini_diorama_01/scene.gltf" --scale 0.1 -o pica
 %BAKE% --scene "assets/meshes/floor/scene.gltf" --scale 1.0 -o floor
-%BAKE% --scene "assets/meshes/testball/scene.gltf" --scale 1.0 -o testball
 %BAKE% --scene "assets/meshes/cornell_box/scene.gltf" --scale 2.0 -o cornell_box
 %BAKE% --scene "assets/meshes/gas_stations_fixed/scene.gltf" --scale 0.005 -o gas_stations
 %BAKE% --scene "assets/meshes/viziers_observation_deck/scene.gltf" --scale 0.0075 -o viziers
