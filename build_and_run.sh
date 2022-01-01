dccf1a46c09a2581f62097a0a588ef9c957aaa61
Ashley <ashley.ruglys@gmail.com>
2022-01-01 22:07:52 +0100

    Adjust script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,4 @@
-cargo run --bin view --release -- --scene $* --width 1920 --height 1080 --no-debug
+cargo run --bin view --no-default-features --release -- --scene $* --width 1280 --height 720 --no-debug
 # cargo run --bin view --release -- --scene pica --width 1920 --height 1080 --no-debug
 
 # --width 2560 --height 1440
