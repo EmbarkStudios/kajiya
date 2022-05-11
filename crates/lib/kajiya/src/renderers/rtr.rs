bb08c21a0c3b2f425a0ce9b31afdad765e4b20d3
Tomasz Stachowiak <h3@h3.gd>
2022-05-11 22:38:31 +0200

    rtr.irradiance f32 -> f16

--- crates/lib/kajiya/src/renderers/rtr.rs
+++ crates/lib/kajiya/src/renderers/rtr.rs
@@ -181,7 +181,7 @@ impl RtrRenderer {
                 self.temporal_irradiance_tex.get_output_and_history(
                     rg,
                     Self::temporal_tex_desc(gbuffer_desc.half_res().extent_2d())
-                        .format(vk::Format::R32G32B32A32_SFLOAT),
+                        .format(vk::Format::R16G16B16A16_SFLOAT),
                 );
 
             let (mut reservoir_output_tex, mut reservoir_history_tex) =
